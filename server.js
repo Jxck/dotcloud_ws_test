68e0591341b398b9d98129e0bff1629c1ecf2134
Jxck <block.rxckin.beats@gmail.com>
2012-04-01 00:13:55 +0900

    add express server & socket.io server

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-var connect = require('connect');
+var express = require('express')
+  , io = require('socket.io')
+  ;
 
-connect().use(function(req, res) {
-  res.end('hello dotcloud :)')
-}).listen(8080);
+var app = module.exports = express.createServer();
+
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+});
+
+io = io.listen(app);
+
+io.sockets.on('connection', function (socket) {
+  console.log('connected');
+  socket.on('msg send', function (msg) {
+    socket.emit('msg push', msg);
+    socket.broadcast.emit('msg push', msg);
+  });
+  socket.on('disconnect', function() {
+    console.log('disconnected');
+  });
+});
+
+app.listen(8080);
