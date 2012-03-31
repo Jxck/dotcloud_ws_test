689ae6a24a60db90da6b24c9389a5862861008d0
Jxck <block.rxckin.beats@gmail.com>
2012-04-01 00:17:59 +0900

    add transport output

--- public/javascripts/sample.js
+++ public/javascripts/sample.js
@@ -2,6 +2,7 @@ var socket = io.connect();
 
 socket.on('connect', function() {
   console.log('connected');
+  console.log('transport is', socket.socket.transport);
   socket.emit('msg send', 'data');
   socket.on('msg push', function (msg) {
     console.log(msg);
